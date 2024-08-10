20cb45a2b1585a962681c37ea893948fe203763c
Shinnanda0707 <72864121+Shinnanda0707@users.noreply.github.com>
2024-08-10 17:26:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import pygame
-import pygame.midi
 pygame.init()
 pygame.mixer.init()
 pygame.font.init()
-pygame.midi.init()
 
 
 def main_loop():
-    rythm = [
-        [
-            [
-                [] for _ in range(4)
-            ] for _ in range(4)]
-            for _ in range(4)
-    ]
+    rythm = [[[] for _ in range(4)]for _ in range(4)]
 
-    win = pygame.display.set_mode((216, 400))
+    win = pygame.display.set_mode((800, 800))
     pygame.display.set_caption("Synth")
     w, h = win.get_size()
     bt_size_x, bt_size_y = w / 4, h * 27 / 200
@@ -24,8 +16,8 @@ def main_loop():
     font = pygame.font.SysFont("Consolas", int(bt_size_y // 4))
     hotbar_inf = [
         font.render("D", True, (0, 0, 0)),
-        font.render("V", True, (0, 0, 0)),
-        font.render("L", True, (0, 0, 0)),
+        font.render("R", True, (0, 0, 0)),
+        font.render("C", True, (0, 0, 0)),
         font.render("S", True, (0, 0, 0))
     ]
     topbar_inf = [
@@ -36,101 +28,77 @@ def main_loop():
     ]
 
     sound = [[pygame.mixer.Sound(f"./drum/{i}{j}.wav") for j in range(4)] for i in range(4)]
-    vo_sound = [[pygame.mixer.Sound(f"./vocal/{i}{j}.wav") for j in range(4)] for i in range(4)]
-
-    mode_list = ["Drum", "Vocal", "Load", "Save"]
     mode = "Drum"
+    rec_start = False
 
-    drum_select = True
     drum_pth = (0, 0)
     drum_sequence = []
-    vocal_rec_started = False
 
     clock = pygame.time.Clock()
     run = True
-    fps = 20
+    fps = 5
     cnt = 0
 
     while run:
         clock.tick(fps)
         win.fill((0, 0, 0))
-        global_col, global_row, global_beat = (cnt // 16) % 4, (cnt // 4) % 4, cnt % 4
-        s_source = rythm[global_col][global_row][global_beat]
-        if len(s_source) > 0:
-            for i in s_source:
-                if i[0] == "v":
-                    vo_sound[i[1]][i[2]].play()
-                else:
-                    sound[i[0]][i[1]].play()
+        global_col, global_row = (cnt // 4) % 4, cnt % 4
+        s_source = rythm[global_col][global_row]
+        for i in s_source:
+            sound[i[0]][i[1]].play()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-            if (event.type == pygame.FINGERDOWN) or (event.type == pygame.MOUSEBUTTONDOWN):
+            if event.type == pygame.FINGERDOWN:
                 mouse_pos = pygame.mouse.get_pos()
                 x = int(mouse_pos[0] // bt_size_x)
                 y = int((mouse_pos[1] - bt_start_y) // bt_size_y)
 
                 if y == 4:
-                    if (mode == "Drum") and (mode_list[x] == "Drum"):
-                        if not drum_select:
+                    if x == 0:
+                        mode = "Drum"
+                        drum_sequence = []
+                    if x == 1:
+                        mode = "Record"
+                        if rec_start:
                             for ds in drum_sequence:
                                 pth, r, c = ds
-                                if pth in rythm[c][r][0]:
-                                    rythm[c][r][0].remove(pth)
+                                if pth in rythm[c][r]:
+                                    rythm[c][r].remove(pth)
                                 else:
-                                    rythm[c][r][0].append(pth)
+                                    rythm[c][r].append(pth)
                             drum_sequence = []
-                        drum_select = not drum_select
-                    elif (mode == "Vocal") and (mode_list[x] == "Vocal"):
-                        vocal_rec_started = not vocal_rec_started
+                        rec_start = not rec_start
                     elif x == 2:
-                        pass # Load
+                        for r in rythm:
+                            for c in r:
+                                c.remove(pth)
                     elif x == 3:
                         with open("./log.txt", "a", encoding="UTF-8") as f:
                             f.write(f"{rythm}\n")
                             f.close()
-                    else:
-                        mode = mode_list[x]
                 elif y == -1:
                     if x == 0:
-                        fps -= 1 / 15
+                        fps -= 1 / 60
                     elif x == 1:
-                        fps += 1 / 15
+                        fps += 1 / 60
                     elif x == 2:
-                        if mode == "Drum":
-                            v = sound[drum_pth[0]][drum_pth[1]].get_volume()
-                            if v > 0:
-                                sound[drum_pth[0]][drum_pth[1]].set_volume(v - 0.1)
-                        elif mode == "Vocal":
-                            for vocals_row in vo_sound:
-                                for vocal_item in vocals_row:
-                                    v = vocal_item.get_volume()
-                                    if v > 0:
-                                        vocal_item.set_volume(v - 0.1)
-                        
+                        v = sound[drum_pth[0]][drum_pth[1]].get_volume()
+                        if v > 0:
+                            sound[drum_pth[0]][drum_pth[1]].set_volume(v - 0.1)
+                    
                     else:
-                        if mode == "Drum":
-                            v = sound[drum_pth[0]][drum_pth[1]].get_volume()
-                            if v < 1:
-                                sound[drum_pth[0]][drum_pth[1]].set_volume(v + 0.1)
-                        elif mode == "Vocal":
-                            for vocals_row in vo_sound:
-                                for vocal_item in vocals_row:
-                                    v = vocal_item.get_volume()
-                                    if v < 1:
-                                        vocal_item.set_volume(v - 0.1)
+                        v = sound[drum_pth[0]][drum_pth[1]].get_volume()
+                        if v < 1:
+                            sound[drum_pth[0]][drum_pth[1]].set_volume(v + 0.1)
+            
                 elif y >= 0:
-                    if mode == "Drum":
-                        if not drum_select:
-                            drum_sequence.append((drum_pth, x, y))
-                        else:
-                            drum_pth = y, x
-                            sound[y][x].play()
-                    elif mode == "Vocal":
-                        vo_sound[y][x].play()
-                        if vocal_rec_started:
-                            rythm[global_col][global_row][global_beat].append(("v", y, x))
+                    if mode == "Record":
+                        drum_sequence.append((drum_pth, x, y))
+                    else:
+                        drum_pth = y, x
+                        sound[y][x].play()
         
         for row in range(6):
             for col in range(4):
@@ -143,25 +111,22 @@ def main_loop():
                     pygame.draw.rect(win, (200, 200, 200), (bt_size_x * col + 1, bt_start_y - bt_size_y, bt_size_x - 2, bt_size_y - 2))
                     win.blit(topbar_inf[col], (bt_size_x * col + 3, bt_start_y - bt_size_y))
         
-        if (mode == "Drum") and not drum_select:
+        if mode == "Record":
             for r in range(4):
                 for c in range(4):
-                    if drum_pth in rythm[r][c][0]:
+                    if drum_pth in rythm[r][c]:
                         pygame.draw.rect(win, (90, 220, 90), (bt_size_x * c + 1, bt_start_y + bt_size_y * r, bt_size_x - 2, bt_size_y - 2))
         pygame.draw.rect(win, (0, 190, 190), (bt_size_x * global_row + 1, bt_start_y + bt_size_y * global_col, bt_size_x - 2, bt_size_y - 2))
         
-        bpm = font.render(f"BPM: {round(fps * 15)}", True, (255, 255, 255))
+        bpm = font.render(f"BPM: {round(fps * 60)}", True, (255, 255, 255))
         md = font.render(f"Mode: {mode}", True, (255, 255, 255))
-        md_2 = font.render(f"Rec: {vocal_rec_started or (not drum_select)}", True, (255, 255, 255))
-
+        
         win.blit(bpm, (5, bt_size_y // 4 * 0.2))
         win.blit(md, (5, bt_size_y // 4 * 1.2))
-        win.blit(md_2, (5, bt_size_y // 2 * 1.1))
 
         pygame.display.update()
 
         cnt += 1
-    pygame.midi.quit()
 
 
 if __name__ == "__main__":
